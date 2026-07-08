181352ac5f902479301caadcf8e3882bb75a40d7
sulay zhang <1085038229@qq.com>
2026-07-08 09:08:48 +0800

    Add files via upload

--- index.config.js
+++ index.config.js
@@ -1,10 +1,10 @@
-
+var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (目标, 全部) => {
-  对于 (变量 名称 在 所有)
-    __defProp(目标, 名称, { 获取: 所有[名称], 可枚举: true });
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
 };
 var __copyProps = (to, from, except, desc) => {
   if (from && typeof from === "object" || typeof from === "function") {
@@ -12,7 +12,7 @@ var __copyProps = (to, from, except, desc) => {
       if (!__hasOwnProp.call(to, key) && key !== except)
         __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
   }
-  返回 到;
+  return to;
 };
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
